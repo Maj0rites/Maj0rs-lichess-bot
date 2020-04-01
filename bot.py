1836db4f2cb17e890412448ef82a30b1938c8556
Maj0rites <52654516+Maj0rites@users.noreply.github.com>
2020-04-01 14:58:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+#
+
+
 import lichess.api
 import random
 import discord
 from discord.ext import commands
 import time
 import asyncio
+import ssl
+import aiohttp
+from collections import Counter
 
 # id = 559392094351917076
 messages = 0
@@ -43,6 +51,72 @@ async def update_stats():
 
 channel_id = 0
 print(repr(token))
+
+extensions = [
+    "cogs.events",
+    "cogs.admin",
+    "cogs.mod",
+    "cogs.info",
+    "cogs.fun",
+    "cogs.misc",
+    "cogs.help",
+]
+
+def get_prefix(bot, msg):
+    user_id = bot.user.id
+    return [f"<@!{user_id}> ", f"<@{user_id}> "] + config.prefix
+
+
+custom_context = ssl.create_default_context()
+conn = aiohttp.TCPConnector(ssl=custom_context)
+
+
+class YMYBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=get_prefix, description=description, case_insensitive=True,
+        )
+
+        self.uptime = ""
+        self.embed_color = 0x36393F
+        self.owner_ids = set(config.owner_ids)
+        self.session = aiohttp.ClientSession(loop=self.loop, connector=conn)
+
+        self._auto_spam_count = Counter()
+        self.spam_control = commands.CooldownMapping.from_cooldown(
+            10, 12.0, commands.BucketType.user
+        )
+
+        for cog in extensions:
+            try:
+                self.load_extension(cog)
+            except Exception as exc:
+                print(f"{cog} {exc.__class__.__name__}: {exc}")
+
+        #
+        # jishaku: A debugging and testing cog for discord.py rewrite bots.
+        # Source code: https://github.com/Gorialis/jishaku
+        #
+
+        self.load_extension("jishaku")
+
+    @property
+    def owners(self):
+        return [self.get_user(i) for i in self.owner_ids]
+
+    @property
+    def config(self):
+        return __import__("config")
+
+    async def on_resumed(self):
+        print("Resumed...")
+
+    async def close(self):
+        await super().close()
+        await self.session.close()
+
+    def run(self):
+        super().run(config.token, reconnect=True)
 
 @client.event
 async def on_ready():
